91c639f87d774bc646597d0cbb5e9372365ddbae
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-29 17:54:32 +0700

    ADD simple server

---
+++ server.js
@@ -0,0 +1,12 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('Hello, ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/test-param/:name', respond);
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
