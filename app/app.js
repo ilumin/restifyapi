4242311d49ef96a444c3ffccafd993e398ffdf1d
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-30 15:48:10 +0700

    UPDATE app use es6 module

---
+++ app/app.js
@@ -0,0 +1,18 @@
+import restify from 'restify'
+
+class App {
+  constructor() {
+    let port = process.env.port || 3000
+
+    this.server = restify.createServer()
+    this.server.get('/test-param/:name', function (req, res, next) {
+      res.send('Hello, ' + req.params.name)
+      next()
+    })
+    this.server.listen(port, (err) => {
+      console.log('%s listening at %s', this.server.name, this.server.url);
+    });
+  }
+}
+
+export default App;
